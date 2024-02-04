16912b1bedd05433249caa10e568847315eb4147
mscha <m.saleh.sedeghi@gmail.com>
2024-02-04 10:47:51 +0330

    Initial commit

---
+++ src/main/java/org/example/HandOn1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class HandOn1 {
+    public static void main(String[] args) {
+        String name = "MohammadSaleh";
+        int age = 20;
+        float score = 15.25f;
+        char scoreByAlfabet = 'B';
+        boolean isMarried = false;
+
+    }
+}
